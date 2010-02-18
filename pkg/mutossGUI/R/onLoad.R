0f7e15d34bb6fb183b8d43ff1f89b3c50bea5c28
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-02-18 08:36:44 +0000

    We do not want to start GUI when the package is loaded.

--- pkg/mutossGUI/R/onLoad.R
+++ pkg/mutossGUI/R/onLoad.R
@@ -2,6 +2,5 @@
   .jpackage(pkgname)
   ## we supply our own JavaGD class
   .setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
-  .setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")
-  mutossGUI() # TODO This line has to be removed for the final release.
+  .setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
 }  
