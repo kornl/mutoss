236ba86954149c27b666071bbe56ebbd3a49edb9
Thorsten Dickhaus <dickhaus@math.hu-berlin.de>
2010-03-11 13:51:24 +0000

    updated by Thorsten

--- www/index.php
+++ www/index.php
@@ -33,8 +33,56 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<p>Mutoss (multiple hypothesis testing open source system) aims at providing a unified, extensible interface covering a large spectrum of multiple hypothesis testing procedures in R.
-It features a graphical user interface and a simulation tool.</p> 
+<p>&#956;TOSS (multiple hypotheses testing in an open software system) aims at providing a unified, extensible interface covering a large spectrum of multiple hypothesis testing procedures in R.<br>
+It features a graphical user interface and a simulation tool.<br>
+
+<table border=0  bgcolor=#FFFFFF align=center>
+<tr>
+<td><a href="./PASCAL2.png"><img src="./PASCAL2.png" align="right" height="165" width="517"></a></td>
+</tr>
+</table>
+
+<h3 align=center>General project information</h2>
+
+<table border=1  bgcolor=#FFFFFF align=center>
+  <tr>
+     <td>Coordinators</td>
+     <td>Dr. Thorsten Dickhaus, Berlin Institute of Technology (dickhaus@cs.tu-berlin.de)<br>
+Dr. Gilles Blanchard, Weierstrass Institute for Applied Analysis and Stochastics (WIAS) Berlin (blanchar@wias-berlin.de)<br>
+Prof. Dr. Klaus-Robert M&uuml;ller, Berlin Institute of Technology</td> 
+ </tr>
+  <tr>
+     <td>Participants</td>
+     <td>
+     Niklas Hack, Medical University of Vienna<br>
+     Dr. Frank Konietschke, Georg-August-University G&ouml;ttingen<br>
+     Kornelius Rohmeyer, Leibniz University Hannover<br>
+     Jonathan Rosenblatt, Tel Aviv University<br>
+     Marsel Scheer, German Diabetes Center D&uuml;sseldorf<br>
+     Wiebke Werft, German Cancer Research Center Heidelberg</td>
+  </tr>
+  <tr>
+     <td>Hosting site</td>
+     <td>Berlin Institute of Technology, Faculty IV, Machine Learning Department</td>
+  </tr>
+  <tr>
+     <td>Coding period</td>
+     <td>January 18th - February 12th, 2010</td>
+  </tr>
+<tr>
+     <td>Project Workshop</td>
+     <td>February 15th - February 16th, see <a href="http://amiando.com/mutoss.html" target="_blank">Workshop website</a></td>
+  </tr>
+</table>
+
+<h3>Further Reading</h2>
+<ul>
+<LI>Wishlist for requesting methods and data types: see <a href="http://user.cs.tu-berlin.de/~dickhaus/mutoss-wishlist.html" target="_blank">here.</a><br>
+<b>If you have further wishes, you are most welcome to address them to Thorsten Dickhaus via email.</b></LI>
+<LI>Website of the <a href="http://amiando.com/mutoss.html" target="_blank">&#956;TOSS Workshop</a></LI>
+<LI>Official &#956;TOSS Website (under construction): <a href="http://www.mutoss.eu" target="_blank">www.mutoss.eu</a></LI>
+</ul>
+</p> 
 
 <!-- <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
 $contents = '';
