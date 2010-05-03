05f45a1d6e2d6a38dbee7bb5b65c812a29e81c66
Gilles Blanchard <gilles.blanchard@math.uni-potsdam.de>
2010-05-03 15:57:52 +0000

    mutoss.apply : added default behavior for label , command history recording

--- pkg/mutoss/R/mutossApply.R
+++ pkg/mutoss/R/mutossApply.R
@@ -1,4 +1,6 @@
-mutoss.apply <- function(mutossObj, f, label, ...) {
+mutoss.apply <- function(mutossObj, f, label = deparse(substitute(f)), ...) {
+  
+  
 	params <- list()	
 	for (param in names(formals(f))) {					#runs over all parameters of f
 		if (param %in% slotNames(mutossObj)) {			#checks if parameter name corresponds to a mutoss slot	
@@ -10,13 +12,17 @@ mutoss.apply <- function(mutossObj, f, label, ...) {
 	result <- eval(as.call(c(f,params,...)))			#evaluates function call with extracted parameters	
 	for (param in names(result)) {						
 		if (param %in% slotNames(mutossObj)) {
-			value <- result[param][[1]]								
+                        value <- result[param][[1]]								
 			attr(value, "method.name") <- label			#attaches attributes to 
 			slot(mutossObj, param) <- value				#writes result in corresponding mutoss slots
 		}
-	}	
+	}
+
+
+        mutossObj@commandHistory = c( mutossObj@commandHistory,
+          paste(format( match.call( call = sys.call( sys.parent(1) ))), collapse='')) #write command into history
+
+
 	return(mutossObj)
 }
-
-
 
