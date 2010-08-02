0fb984e608f15360f7946d8bad91a7c418a0d168
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-02 14:29:51 +0000

    For the ease of using r-forge, we remove mutoss again from the dependencies.

--- pkg/mutossGUI/R/onLoad.R
+++ pkg/mutossGUI/R/onLoad.R
@@ -3,4 +3,5 @@
   ## we supply our own JavaGD class
   .setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
   .setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
+  require(mutoss)
 }  
