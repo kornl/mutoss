10cb12211439c0e0c221e55292a13121cbf6586b
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-19 14:32:42 +0000

    Small typo bugfix.

--- pkg/mutossGUI/R/helperfunctions.R
+++ pkg/mutossGUI/R/helperfunctions.R
@@ -3,8 +3,8 @@ mutossGUI.vars <- list()
 startRecording <- function() {
 	mutossGUI.vars$output <<- "output" # TODO These variablenames are stupid! But there was some bug with hidden ones or non-global ones?
 	mutossGUI.vars$errorMsg <<- "errorMsg"
-	mutossGUI.vars$outputCon <<- textConnection(output, open="w")
-	mutossGUI.vars$errorCon <<- textConnection(errorMsg, open="w")
+	mutossGUI.vars$outputCon <<- textConnection(mutossGUI.vars$output, open="w")
+	mutossGUI.vars$errorCon <<- textConnection(mutossGUI.vars$errorMsg, open="w")
 	sink(mutossGUI.vars$outputCon)
 	sink(mutossGUI.vars$errorCon, type="message")
 }
