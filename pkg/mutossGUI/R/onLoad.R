2f1c207b95388c2928c16b9202d9c9e1909b8046
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-09-23 11:44:07 +0000

    CommonJavaJars (>= 1.0.3)

--- pkg/mutossGUI/R/onLoad.R
+++ pkg/mutossGUI/R/onLoad.R
@@ -20,23 +20,13 @@
 		}		
 	}
 	
-	jarsSC <- c("afcommons", "commons-collections", "commons-lang", 
+	jars <- c("afcommons", "commons-collections", "commons-lang", 
 			"commons-logging", "commons-validator", "forms", 
 			"iText", "jhlir.jar", "jxlayer", 
 			"log4j", "mysql-connector-java", 
 			"swing-worker", "poi")
 	
-	jars <- c()	
-	classes <- system.file("java", package = "CommonJavaJars", lib.loc = NULL)
-	files <- list.files(classes, full.names = FALSE)
-	# For now always ignore the jars that require Java >= 6.
-	files <- grep("J6", files, TRUE, value = TRUE, invert = TRUE)
-	for (j in jarsSC) {
-		# Always take the newest jar per default:
-		jars <- c(jars, sort(grep(j, files, TRUE, value = TRUE), decreasing = TRUE)[1])
-	}
-	
-	.jpackage("CommonJavaJars", jars=jars)
+	loadJars(jars)
 	
 	## we supply our own JavaGD class
 	Sys.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")
