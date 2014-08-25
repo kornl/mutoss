f3020ee038c3d9dad2410b176dc1cf29aa41e829
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-08-25 17:18:07 +0200

    Strange format change.

--- pkg/mutossGUI/R/helperfunctions.R
+++ pkg/mutossGUI/R/helperfunctions.R
@@ -23,13 +23,11 @@ stopRecording <- function() {
 }
 
 myContrMat <- function(type,l,df,group) {
-	require(multcomp)
 	n <- table(df[,group])[as.numeric(factor(l,levels=levels(df[,group])))]
 	x <- contrMat(n=n,type=type)
 }
 
 myContrMat <- function(type,l,df,group) {
-	require(multcomp)
 	n <- table(df[,group])[as.numeric(factor(l,levels=levels(df[,group])))]
 	x <- contrMat(n=n,type=type)
 }
